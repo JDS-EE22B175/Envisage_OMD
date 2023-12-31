edd81e4cd046e6a5a34b6cc9d0c6c3ab5c2c4141
JDS-EE22B175 <ee22b175@smail.iitm.ac.in>
2023-12-31 17:01:23 +0530

    Added Sabotage Audio Recording

---
+++ Assets/Scripts/AudioInteractable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioInteractable : MonoBehaviour, IInteractable
+{
+    AudioSource audioSource;
+    [SerializeField] float audioLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Interact(Transform interactorTransform)
+    {
+        audioSource.enabled = true;
+
+        interactorTransform.gameObject.GetComponent<PlayerMovement>().canMove = false;
+
+        StartCoroutine(ListenToAudio(interactorTransform));
+    }
+
+    IEnumerator ListenToAudio(Transform interactorTransform)
+    {
+        yield return new WaitForSeconds(audioLength);
+        audioSource.enabled = false;
+
+        interactorTransform.gameObject.GetComponent<PlayerMovement>().canMove = true;
+    }
+    public string GetInteractText()
+    {
+        return "Listen To The " + gameObject.name;
+    }
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
